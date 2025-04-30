fc9093e884b33e7056b32dbc70a8049a81215c50
Bhanuprakash lagishety <bhanuprakash.lagishetty@gmail.com>
2025-04-30 15:28:35 +0530

    conent

--- src/Components/single/Single.jsx
+++ src/Components/single/Single.jsx
@@ -7,7 +7,7 @@ function Single() {
   return (
     <div className="single">
         <SinglePost></SinglePost>
-        <div className="sidebarhome container ">
+        <div className="sidebarhome container  content">
 
         <Sidebar></Sidebar>
         </div>
